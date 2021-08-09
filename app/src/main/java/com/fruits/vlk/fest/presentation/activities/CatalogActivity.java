a97a7ec96c821a46db1bc5fe0851c45beb391a88
BuenasDias <tmaksim1989@gmail.com>
2021-08-09 19:20:34 +0300

    Check

---
+++ app/src/main/java/com/fruits/vlk/fest/presentation/activities/CatalogActivity.java
@@ -0,0 +1,16 @@
+package com.fruits.vlk.fest.presentation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fruits.vlk.fest.R;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+    }
+}
